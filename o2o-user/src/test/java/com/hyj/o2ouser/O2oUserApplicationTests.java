a6eabf55ff1c754462a7445059fd99db83d14e90
hyj <hanyoujin@thunisoft.com>
2020-04-29 14:12:17 +0800

    增加新项目

---
+++ o2o-user/src/test/java/com/hyj/o2ouser/O2oUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyj.o2ouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O2oUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
