4eb1f3fd4947ab90b2be3589be15446b9c4bfdab
Youqianhan <15998451842>
2021-01-04 15:51:03 +0800

    ADD project

---
+++ yjmall-product/src/main/java/com/hyj/yjmall/product/YjmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.hyj.yjmall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjmallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjmallProductApplication.class, args);
+    }
+
+}
---
+++ yjmall-product/src/test/java/com/hyj/yjmall/product/YjmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyj.yjmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YjmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
